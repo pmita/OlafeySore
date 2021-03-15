b774bee2bcbbf1f3bfc20cf2542312114dea538e
pmita <panos.mita2@outlook.com>
2021-03-15 15:17:46 +0000

    js pop up modal added

--- app.js
+++ app.js
@@ -10,3 +10,21 @@ function animateBurger(){
 }
 
 animateBurger();
+
+//Pop up modal
+const modalContainer = document.querySelector('.modal-container');
+const modalClose = document.querySelector('.button-x');
+
+if(modalContainer){
+    //Remove modal when user clicks x
+    modalClose.addEventListener('click', () =>{
+        modalContainer.classList.remove('modal-container-active');
+    });
+
+    //Initiate pop up window after 1s of screen time
+    window.addEventListener('load', ()=>{
+        setTimeout(()=>{
+            modalContainer.classList.add('modal-container-active');
+        }, 1000)
+    });
+}
